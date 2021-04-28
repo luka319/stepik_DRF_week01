55cdf5e6dabd3b3a3ffe059ec791c5f3a8fb513a
Luka Pacholli <luka319@i.ua>
2021-04-28 18:50:48 +0300

    05 bad update_or_create

--- items/management/commands/import_items.py
+++ items/management/commands/import_items.py
@@ -2,6 +2,13 @@ import requests
 from django.core.management import BaseCommand
 from items.models import Item
 
+f=open(r'static/item_foodboxes.json',"wb") #открываем файл для записи, в режиме wb
+ufr = requests.get("https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json") #делаем запрос
+f.write(ufr.content) #записываем содержимое в файл;
+f.close()
+
+
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -10,10 +17,15 @@ class Command(BaseCommand):
 
         for item in response.json():
             try:
-                Item.objects.update_or_create(self, title=item['title'], defaults=None)
+                Item.objects.update_or_create(self, title=item['title'],
+                                              description=item['description'],
+                                              defaults={'title': 'вот так'} )
+                # Item.objects.update_or_create(title=item['title'],
+                #                               description=item['description'],
+                #                               defaults={'title': 'вот так'} )
                 # Item.objects.create(self, title=item['title'])
             except ValueError:
-                Item.objects.update_or_create(self, title="что-то не то", defaults=None)
+                Item.objects.update_or_create(self, title="что-то не то", defaults={'title': 'не так'})
                 # Item.objects.create(self, title="что-то не то")
             finally:
                 Item.objects.update_or_create(self,
@@ -24,6 +36,6 @@ class Command(BaseCommand):
                     image=item['image'],
                     weight=item['weight_grams'],
                     price=item['price'],
-
+                    defaults=None,
                 )
 
